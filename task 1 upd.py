752c1a0ddf18b3dd3995fba3e7d08a483ebc9d3e
Iryna Hordiienko <irynagordiyenko9@gmail.com>
2025-11-03 13:42:00 +0200

    task 1.py

---
+++ task 1 upd.py
@@ -0,0 +1,110 @@
+from collections import UserDict
+from datetime import timedelta, datetime
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Ім'я не може бути порожнім")
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Телефон має містити рівно 10 цифр")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return True
+        return False
+
+    def edit_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[idx] = Phone(new_phone)
+                return True
+        return False
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+        return None
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
+    def __str__(self):
+        phones_str = '; '.join(p.value for p in self.phones)
+        birthday_str = f", birthday: {self.birthday.value}"if self.birthday else""
+        return f"Contact name: {self.name.value}, phones: {phones_str}{birthday_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            return True
+        return False
+
+    def get_upcoming_birthdays(self):
+        today = datetime.today().date()
+        upcoming_birthdays = []
+
+        for record in self.data.values():
+            if record.birthday:
+                bday = datetime.strptime(record.birthday.value, "%d.%m.%Y").date()
+                bday_this_year = bday.replace(year=today.year)
+
+                if bday_this_year < today:
+                    bday_this_year = bday_this_year.replace(year=today.year + 1)
+
+                days_diff = (bday_this_year - today).days
+                if 0 <= days_diff <= 6:
+                    congratulation_date = bday_this_year
+                    if congratulation_date.weekday() == 5:  # субота
+                        congratulation_date += timedelta(days=2)
+                    elif congratulation_date.weekday() == 6:  # неділя
+                        congratulation_date += timedelta(days=1)
+                    upcoming_birthdays.append({
+                        "name": record.name.value,
+                        "congratulation_date": congratulation_date.strftime("%d.%m.%Y")
+                    })
+        return upcoming_birthdays
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            self.date = datetime.strptime(value, "%d.%m.%Y")
+            super().__init__(value)
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
